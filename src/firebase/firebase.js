ae6009d67f68473f57b4ef1c3b0e43bbce705462
deraolisah <deraolisah@gmail.com>
2025-02-14 15:38:25 +0100

    Updates

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -16,20 +16,18 @@
 // export default app;
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAfvYIGZT7z_c9dkWQXsXf1obMDpq2EJVI",
-  authDomain: "trendmart-bb0c2.firebaseapp.com",
-  projectId: "trendmart-bb0c2",
-  storageBucket: "trendmart-bb0c2.firebasestorage.app",
-  messagingSenderId: "920443737688",
-  appId: "1:920443737688:web:3d258a6f851ea8b4155c2d"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
